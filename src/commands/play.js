c5a09a44aa6de85841b07ee1635bf86942dd7647
RedHeadRedemption <66055451+RedHeadRedemption@users.noreply.github.com>
2021-10-05 19:37:27 +0300

    UPDATE play.js with modes and corresponding cooldowns
    starting session command gives 3 options for 3 cooldown modes until the message is deleted

--- src/commands/play.js
+++ src/commands/play.js
@@ -4,12 +4,32 @@ const {SlashCommandBuilder} = require('@discordjs/builders');
 module.exports = {
     data : new SlashCommandBuilder()
         .setName('playhard')
-        .setDescription('Start a new playing session!'),
+        .setDescription('Start a new playing session!')
+        .addStringOption(option => 
+            option.setName('mode')
+            .setDescription('Session difficulty')
+            .setRequired(true)
+            .addChoice('Easy','easy_mode')
+            .addChoice('Normal','normal_mode')
+            .addChoice('Hard','hard_mode')
+        ),
     async execute(interaction) {
+        const mode = interaction.options.getString('mode');
+        let cooldown = 3000;
 
+        if (mode == 'easy_mode') {
+            cooldown = 3000;
+        }
+        else if (mode == 'normal_mode') {
+            cooldown = 6000;
+        }
+        else {
+            cooldown = 9000;
+        }
+        
 
         await interaction.reply("Started Playing");
-        await setTimeout(() => { interaction.deleteReply(); }, 2000 );
+        await setTimeout(() => { interaction.deleteReply(); }, cooldown );
 
     }
     
